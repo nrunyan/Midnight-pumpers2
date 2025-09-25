88b5ec2546f94d4797b5fb9a64c5714661f355fc
valerie <vbarker@unm.edu>
2025-09-25 01:24:36 -0600

    made a test for payment service

---
+++ src/Tests/TestPaymentControl.java
@@ -0,0 +1,30 @@
+package Tests;
+
+import SecondLevel.PaymentControl;
+import Util.CommunicationString;
+
+/**
+ * Another test main, run after starting the GUIs
+ */
+public class TestPaymentControl {
+    /**
+     * starts
+     * @param args no cmd args
+     */
+
+    public static boolean on = true;
+    public static void main (String [] args) {
+        PaymentControl paymentControlTest = new PaymentControl();
+
+        while(on) {
+            try {
+
+                Thread.sleep(2000);
+                //Test some payment control stuff?
+
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
